3f1cfdda8d6fdb18a771fa294b924659a19d6b33
shivkanthb <shivkanthb@gmail.com>
2016-08-22 17:09:28 -0700

    simple tests for models

--- courses/tests.py
+++ courses/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
 
+from django.utils import timezone
+
 # Create your tests here.
+
+from .models import Course
+
+class CourseModelTests(TestCase):
+	def test_course_creation(self):
+		course = Course.objects.create(
+			title="test title 1",description="test title description"
+		)
+		now = timezone.now()
+		self.assertLess(course.created_at,now)
